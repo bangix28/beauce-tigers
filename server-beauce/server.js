c89ceced93c7d315ffc89ce7dd6ad0b2f95f5259
Granger kénolane <kenolane28@gmail.com>
2024-10-18 23:13:51 +0200

    Ajout  du SSL

--- server-beauce/server.js
+++ server-beauce/server.js
@@ -3,18 +3,18 @@ const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 const token_strapi = process.env.TOKEN_STRAPI;
-const routes_strapi =  process.env.ROUTES_STRAPI
+const routes_strapi = process.env.ROUTES_STRAPI;
 const dotenv = require('dotenv');
 const Router = require("./routeur");
 const RouteurApi = require("./routeur");
 const algoliasearch = require("algoliasearch");
+const fs = require('fs');
+const https = require('https');
+
 dotenv.config();
-
-
 
 app.use(express.json());
 app.use(cors());
-
 
 app.get('/api/riotAccount', async (req, res) => {
     try {
@@ -26,6 +26,17 @@ app.get('/api/riotAccount', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV === 'production') {
+    const options = {
+        key: fs.readFileSync(process.env.SSL_PRIVATE_KEY_PATH),
+        cert: fs.readFileSync(process.env.SSL_PRIVATE_CERT_PATH)
+    };
+
+    https.createServer(options, app).listen(PORT, () => {
+        console.log(`Server is running on port ${PORT} with SSL`);
+    });
+} else {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT} without SSL`);
+    });
+}
