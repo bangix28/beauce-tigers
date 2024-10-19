b0af91c436960caf4bc9568267505b4437d90bfb
Granger kénolane <kenolane28@gmail.com>
2024-10-19 20:44:54 +0200

    Ajout du winrate dans le leaderboard

--- beauce-tigers/src/views/Accueil.vue
+++ beauce-tigers/src/views/Accueil.vue
@@ -5,18 +5,17 @@
     >
       <h1>Leaderboard</h1>
     </div>
-
-
     <!--Tableau-->
     <h2 class="mb-3 ms-5 col-12">Classement</h2>
     <div id="classement" class="container fs-5">
       <div v-for="(player, index) in listSummoner" :key="index" class="row align-items-center justify-content-center mt-3">
         <div class="col-12 col-sm-1 fs-3 text">#{{index + 1}}</div>
         <div class="col-12 col-sm-2 summonerName text">{{player.summonerName}}</div>
-        <div class="col-12 col-sm-3 rank text">{{player.summonerRankedSoloRank || 'non classée'}} {{player.summonerRankedSoloTier || ''}}</div>
+        <div class="col-12 col-sm-2 rank text">{{player.summonerRankedSoloTier || ''}} {{player.summonerRankedSoloRank || 'non classée'}}</div>
         <template v-if="player.summonerRankedSoloLeaguePoints">
           <div class="col-12 col-sm-2 lp text">{{player.summonerRankedSoloLeaguePoints || ''}} LP</div>
-          <div class="col-12 col-sm-4 winLoose text-sm-end fs-6 text">Win {{player.summoner_ranked_solo_wins || 0}}  / Loose {{player.summonerRankedSoloLosses || 0}}</div>
+          <div class="col-12 col-sm-2 summonerName text" > Winrate: {{ calculateWinrate(player.summoner_ranked_solo_wins, player.summonerRankedSoloLosses) }}%</div>
+          <div class="col-12 col-sm-2 winLoose text-sm-end fs-6 text">Win {{player.summoner_ranked_solo_wins || 0}}  / Loose {{player.summonerRankedSoloLosses || 0}}</div>
         </template>
       </div>
     </div>
@@ -34,21 +33,25 @@ export default {
   },
   mounted() {
     this.fetchData()
-    console.log(this.listSummoner)
   },
   methods:{
     async fetchData() {
       let urlToFetch  = import.meta.env.VITE_RIOT_ACCOUNT_URL;
       axios.get(urlToFetch)
         .then(response => {
-          console.log(response.data);
           this.listSummoner = response.data.member.sort((a, b) => b.score - a.score);
         })
         .catch(error => {
           console.error(error);
         });
     },
-  }
+    calculateWinrate(wins, losses) {
+      const winsInt = parseInt(wins, 10);
+      const lossesInt = parseInt(losses, 10);
+      const totalGames = winsInt + lossesInt;
+      return totalGames > 0 ? ((winsInt / totalGames) * 100).toFixed(2) : 0;
+    }
+  },
 }
 </script>
 
